6800d3fbbec1b0a5f1c3ed31edf1304f1249687f
Spencer Cambern <cambern134@gmail.com>
2020-05-02 02:52:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
- square_array = []
-array.each { |element| squared << element ** 2 }
-squared
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
